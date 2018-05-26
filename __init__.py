742fdcafa380d1078985dd569b77e70a1ad8e4ae
johnvictorfs <johnvictorfs@gmail.com>
2018-05-26 17:23:33 -0300

    Added __init__.py.

---
+++ __init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from clearScreen import clear_screen
+from continueScreen import continue_screen
+
+"""
+Sequel of my terminal text-RPG game originally made in C.
+But this time made in Python.
+
+Objectives:
+- To use as little non-standard resources as possible.
+- Run on as many machines as possible.
+- Learn stuff.
+- Have fun.
+"""
+
+__author__  = "John Victor"
+__version__ = "0.1"
+__license__ = "MIT License"
+
+
+def initial_screen():
+    clear_screen()
+    print("Welcome to Sacred River 2!")
+    print("\n")
+    print("Made by {} -  Version: {}".format(__author__, __version__))
+    print("\n")
+    print("The sequel of my terminal text-RPG originally made in C. This time made in Python.")
+    print("\n")
+    continue_screen()
+    return
--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python
-
-"""
-Sequel of my terminal text-RPG game originally made in C.
-But this time made in Python.
-
-Objectives:
-- To use as little non-standard resources as possible.
-- Run on as many machines as possible.
-- Learn stuff.
-- Have fun.
-"""
-
-__author__ = 'John Victor'
-__version__ = '0.1'
-
+# Local
 from classes import *
 from userInventory import *
 from gameMenu import game_menu
-from clearScreen import clear_screen
 from setUsername import set_username
-from continueScreen import continue_screen
 from killGame import kill_game
+from __init__ import *
 
 inventory = Inventory()
 
@@ -29,7 +14,7 @@ Notes:
     # Clearing screen
 clear_screen()
 
-    # Press Enter to continue screen
+    # 'Press Enter to continue' screen
 continue_screen()
 
     # Giving item to player
@@ -38,18 +23,6 @@ inventory.add_item(item_name)
     # Removing item from player
 inventory.remove_item(item_name)
 """
-
-
-def initial_screen():
-    clear_screen()
-    print("Welcome to Sacred River 2!")
-    print("\n")
-    print("Made by {} -  Version: {}".format(__author__, __version__))
-    print("\n")
-    print("The sequel of my terminal text-RPG originally made in C. This time made in Python.")
-    print("\n")
-    continue_screen()
-    return
 
 
 def game_loop():
